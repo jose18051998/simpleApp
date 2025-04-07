255c52c3d0a562f1e4f73997cfdbe78530c6ea79
jose <josebenjumedarubio@gmail.com>
2025-04-07 09:21:43 +0200

    Tweaks to compile.sh script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,7 +3,7 @@
 cd $SIMPLE_APP_HOME
 
 javac -d bin src/com/simpleapp/common/SimpleAppInterface.java
-javac -cp bin src/com/simpleapp/server/SimpleAppServer.java
-javac -cp bin src/com/simpleapp/client/SimpleAppClient.java
+javac -cp bin -d bin src/com/simpleapp/server/SimpleAppServer.java
+javac -cp bin -d bin src/com/simpleapp/client/SimpleAppClient.java
 
 echo "Compilation completed."
