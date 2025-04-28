d034fc510740e673e4f8fd952441a057808f9ef4
jose <josebenjumedarubio@gmail.com>
2025-04-28 10:14:27 +0200

    Connecting the server to the simpleapp database

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+DB_CONN_JAR_PATH="$SIMPLE_APP_HOME/lib/postgresql-42.7.5.jar"
+SRC_DIR="$SIMPLE_APP_HOME/src"
+BIN_DIR="$SIMPLE_APP_HOME/bin"
+
 cd $SIMPLE_APP_HOME
 
-javac -d bin src/com/simpleapp/common/SimpleAppInterface.java
-javac -cp bin -d bin src/com/simpleapp/server/SimpleAppServer.java
-javac -cp bin -d bin src/com/simpleapp/client/SimpleAppClient.java
+
+javac -d "$BIN_DIR" "$SRC_DIR/com/simpleapp/common/"*.java
+javac -cp "$BIN_DIR:$DB_CONN_JAR_PATH" -d "$BIN_DIR" "$SRC_DIR/com/simpleapp/server/"*.java
+javac -cp "$BIN_DIR" -d "$BIN_DIR" "$SRC_DIR/com/simpleapp/client/"*.java
 
 echo "Compilation completed."
